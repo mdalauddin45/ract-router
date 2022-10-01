0cd4cf5493e44370d2f94dbe25a15234f3e3bdea
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-01 22:17:40 +0600

    a

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  const router = createBrowserRouter([]);
+  const router = createBrowserRouter([
+    { path: "/home", element: <div>home</div> },
+    { path: "/about", element: <div>about</div> },
+  ]);
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
